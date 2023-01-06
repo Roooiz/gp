4839dfc53732263d65e1c59d17a7ec348e5cfbbe
roooiz <ghostlyeclipse@gmail.com>
2023-01-06 21:12:17 +0300

    Add function: minus

--- first/name.py
+++ first/name.py
@@ -1,5 +1,14 @@
 def plus(a, b):
     return a + b
 
-if __name__=="__main__":
-    print(plus())
+
+text = "new text"
+
+
+def minus(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print(plus(2, 2))
+    print(minus(4, 2))
